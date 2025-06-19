3f0738afbe7c6b537268b4dc1e27714f678b3c5f
Asif Jahan <asifjahan307@gmail.com>
2025-06-20 00:52:03 +0600

    app binding added

--- lib/bindings/app_binding.dart
+++ lib/bindings/app_binding.dart
@@ -1,17 +1,27 @@
 import 'package:get/get.dart';
 import 'package:kaldmv/User/Auth/controller/login_controller.dart';
+import 'package:kaldmv/User/Auth/controller/signup_controller.dart';
+import 'package:kaldmv/User/Views/features/Add_New_Item/controller/add_new_item_controller.dart';
 import 'package:kaldmv/User/Views/features/Bottom_Nav_Bar/controller/bottom_nav_bar_controller.dart';
 import 'package:kaldmv/User/Views/features/Home/controller/drawer_controller.dart';
 import 'package:kaldmv/User/Views/features/Home/controller/header_controller.dart';
 import 'package:kaldmv/User/Views/features/Home/controller/home_controller.dart';
 import 'package:kaldmv/User/Views/features/Home/controller/tsfs_controller.dart';
+import 'package:kaldmv/User/Views/features/Profile/controller/profile_controller.dart';
 import 'package:kaldmv/User/Views/features/Search/controller/search_controller.dart';
+import 'package:kaldmv/User/Views/features/Search/controller/video_controller.dart';
 
 class AppBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(HomeController(), permanent: true);
+    Get.put(AddNewItemController(), permanent: true);
+    Get.put(VideoController(), permanent: true);
+    Get.put(SignupController(), permanent: true);
+    Get.put(ProfileController(), permanent: true);
     Get.put(LoginController(), permanent: true);
+    Get.put(HeaderController(), permanent: true);
+    Get.put(HeaderController(), permanent: true);
     Get.put(SearchhController(), permanent: true);
     Get.put(BottomNavController(), permanent: true);
     Get.put(HeaderController(), permanent: true);
